9efba53aa87a1d1e77676ec7dcf06ac3c037616a
Nate Mills <natemills018@gmail.com>
2023-12-27 13:40:23 -0500

    Aysnc-Drills\

---
+++ scripts.js
@@ -0,0 +1,13 @@
+getWordOfTheDay()
+.then((word) => {
+    console.log(`The word of the day is ${word}.`)
+})
+
+
+function orderLunch(name) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            
+        }
+    }   
+}
