b3b267d42643d2f8f5963092c9e825baf2e4d842
Nate Mills <natemills018@gmail.com>
2023-12-27 22:27:44 -0500

    Starting lab

--- scripts.js
+++ scripts.js
@@ -1,13 +1,25 @@
-getWordOfTheDay()
-.then((word) => {
-    console.log(`The word of the day is ${word}.`)
-})
+// function drillingAsync (message, delay) {
+
+//     setTimeout(() => {
+//         return console.log(message);
+//     }, 2000);
+    
+// }
 
 
-function orderLunch(name) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            
-        }
-    }   
+function getWords (word) {
+    setTimeout(() => {
+        return console.log(`This is the first return ${word}`);
+    }, 1000);
+    setTimeout(() => {
+        return console.log(`This is the second return ${word}`);
+    }, 3000);
+    setTimeout(() => {
+        return console.log(`This is the third return ${word}`);
+    }, 2000);
 }
+
+
+getWords('Giraffe');
+
+// drillingAsync('Today we are drilling aysnc')
